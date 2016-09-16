b51a132380f22424a924d96f21a5567e1c98ca3c
miller-n <miller_n@lanl.gov>
2016-09-16 14:36:47 -0600

    created 2 new perl scripts since Phame.pm functionality was too complex to recreate in python

---
+++ perl_scripts/codingRegions.pl
@@ -0,0 +1,83 @@
+ft;
+my $annotation=shift;
+my $name=shift;
+my $start;
+my $end;
+my $gap_start=1;
+my $gap_end;
+my $source_start=1;
+my $source_end=0;
+my %CDS;
+my $line;
+my $temp;
+
+my $outfile=$dir."/noncoding.txt";
+my $coding=$dir."/CDScoords.txt";
+
+open (OUT,">$outfile");
+open (CDS,">$coding");
+
+my $first=1;
+my $permutation=0;
+
+open (IN, "$annotation")|| die "$!";
+while(<IN>){
+   chomp;
+      if (/##sequence-region/){
+            $permutation=$permutation+$source_end;
+             ($line,$temp,$source_start,$source_end)=split " ",$_;
+     }
+     if (!/^#/){
+           my ($name,$source,$method,$start,$stop,$score,$strand,$phase,$field)=split "\t",$_;
+           my @group=split ";",$field if ($field);
+            if ($method and $method=~/CDS/){
+             $start=$start+$permutation;                        
+             $stop=$stop+$permutation;
+
+             print CDS "$name\t$start\t$stop\t";
+             $CDS{$start}=$stop;
+             foreach (@group){if (/product=(.+)/ || /description=(.+)/){print CDS $1;}}
+             print CDS "\n";
+             }
+        }
+    }
+
+my $prev=0;
+my $last=0;
+foreach my $begin (sort{$a<=>$b} keys %CDS){
+   my $end=$CDS{$begin};
+if ($first){
+      if ($begin==1){$gap_start=$end+1;}
+      else{
+         $gap_end=$begin-1;
+       if ($gap_start<$gap_end){print OUT "$name\t$gap_start\t$gap_end\tnoncoding\n";}
+     }
+    $first = 0;
+    }
+    else {
+    $gap_end = $begin-1;
+    if( $gap_start<$gap_end){print OUT "$name\t$gap_start\t$gap_end\tnoncoding\n";}
+    }
+    if ($begin==$prev){
+    $gap_start=$prev-1;
+    $gap_end=$prev-1;
+    print OUT "$name\t$gap_start\t$gap_end\tnoncoding\n";
+    }
+
+    $gap_start=$end+1;
+    $prev=$end+2;
+    $last=$end;
+}
+
+    if ($last < $source_end){
+    $gap_start = $last+1;
+    print OUT "$name\t$gap_start\t$source_end\tnoncoding\n";
+    }
+}
+
+
+
+
+
+
+
---
+++ perl_scripts/identifyGaps.pl
@@ -0,0 +1,89 @@
+my $dir=shift;
+my $list=shift;
+my $name=shift;
+my $type=shift;
+my $project=shift;
+my $gapdir=$dir.'/gaps';
+my $repeatdir=$dir.'/stats';
+my %query;
+my $line=0;
+my $all_gapfile;
+my $gap_start;
+my $gap_end;
+
+if ($type=~/map/){$all_gapfile="$dir\/$project\_mapping_gaps.txt";}
+elsif ($type=~/snp/){$all_gapfile="$dir\/$project\_all_gaps.txt";}
+open (GAP,">$all_gapfile")||die "$!";
+
+open (LIST,"$list")||die "$!";
+while (<LIST>){
+    chomp;
+    $query{$_}++;
+}
+close LIST;
+
+opendir(DIR,"$gapdir");
+while (my $gaps= readdir(DIR)){
+    if ($gaps=~ /^$name\_norepeats\_(.+)\_norepeats\.gaps/ || $gaps=~ /^$name\_(.+_contig)s?\.gaps/ ||$gaps=~ /^$name\_(.+)\.gaps/ ){
+          if (exists $query{$1}){
+            $line=0;
+            my $gapfile= "$gapdir/$gaps";
+            open (IN,$gapfile)|| die "$!";
+            #print "Nucmer Gaps\n";
+            while (<IN>){$line++; print GAP "$_";}
+            close IN;
+            if ($line ==0){print "Empty File: $gapfile\n";$line=0;}
+        }
+    }
+    if ($type=~/snp/){
+        if ($gaps=~ /^$name\_(.+)\_$name(\_\d+\_\d+)?\.gaps$/){
+            my $query=$1;
+            my @read_types=("_pread","_sread","_read");
+            foreach my $type(@read_types){
+                my $tmp= $query.$type;
+                if (exists $query{$tmp}){
+                    my $gap_file= "$gapdir/$gaps";
+                    $line=0;
+                    open (IN,$gap_file)|| die "$!";
+
+                    while (<IN>){
+                        chomp;
+                        $line++;
+                        next if (/Start\s+End\s+Length.+/);
+                        my ($start,$end,$length,$ref)= split "\t",$_;
+                        if ($ref=~ /$name\_(\d+)\_(\d+)$/){
+                            $gap_start=$start+$1-1;
+                            $gap_end=$1+$end-1;
+                        }
+                        else{
+                            $gap_start=$start;
+                            $gap_end=$end;
+                        }
+                        print GAP "$name\t$gap_start\t$gap_end\t$length\t${query}$type\n";
+                    }
+                    close IN;
+                    if ($line == 1){`rm $gap_file`; $line=0;}
+                }
+         }
+      }
+   }
+ }
+closedir DIR;
+
+opendir(REPEAT,"$repeatdir");
+while (my $repeats= readdir(REPEAT)){
+    if ($repeats=~ /$name\_repeat_coords\.txt/){
+        my $repeatfile= "$repeatdir/$repeats";
+        open (IN,$repeatfile)|| die "$!";
+
+        while (<IN>){
+            chomp;
+            my ($ref,$temp,$start,$end,$length)= split "\t",$_;
+            print GAP "$ref\t$start\t$end\t$length\tREPEATS\n";
+        }
+        close IN;
+    }
+}
+closedir REPEAT;
+return $all_gapfile;
+}
