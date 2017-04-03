ae41d522ab8edfd4e3897caa779ada537ad4f003
Migun Shakya <microbeatic@gmail.com>
2017-04-03 17:40:10 -0600

    thinking of more modification to module

--- phame/__init__.py
+++ phame/__init__.py
@@ -1,1 +1,7 @@
+"""Top level providing shortcuts for useful classes."""
+
 from read_control import ParseFile
+# from GFFUtils import GFFFile
+# from GFFUtils.GTFFile import *
+
+__version__ = "devel"
